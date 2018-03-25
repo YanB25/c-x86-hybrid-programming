3e236ece7e5ae46cfc4847d95fbf17962b26a88c
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-25 12:13:33 +0800

    fix important bugs - forget to pop bp in my_add in frame steup

--- utilities.asm
+++ utilities.asm
@@ -4,12 +4,14 @@ global hello_hybrid_programming
 global my_add
 
 clear_screen:
+    ; 清屏
     mov ax, 03h
     int 10h
     pop ecx
     jmp cx
 
 hello_hybrid_programming:
+    ; 在屏幕上打印 hello hybrid programming
     mov ah, 13H
     mov al, 1
     mov bl, 0FH
@@ -24,13 +26,15 @@ hello_hybrid_programming:
     jmp cx
 
 my_add:
+    ;将参数一和参数二相加，再返回
     push bp
     mov bp, sp
-    ; sub sp, 0 ; 如果要在栈上分配空间，把这句的0改成所分配的字节数
+    sub sp, 0 ; 如果要在栈上分配空间，把这句的0改成所分配的字节数
 
     mov ax, [bp + 6]
     add ax, [bp + 10]
     
+    pop bp
 
     pop ecx
     jmp cx
